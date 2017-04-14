cc8ad57d054fc4c0130e040b67e0c7bd264eece7
David Freifeld <david.freifeld@analytics.adsrvr.org>
2017-04-14 03:54:03 +0000

    functionality for randomly setting the key

--- Game.py
+++ Game.py
@@ -5,19 +5,39 @@ import numpy as np
 
 class Game:
 
+    key_set = np.concatenate((np.repeat('blue', 9), np.repeat('red', 8), np.repeat('neutral', 7), ['assassin']))
+
     def __init__(self):
         # randomly generate some words and the key
-        self.words = ['tree', 'apple', 'circus', 'pig', 'face', 'mug', 'wind', 'stick', 'basket', 'picnic', 'string',
-                      'cloud', 'mouth', 'candy', 'fridge', 'table', 'iron', 'spoon', 'blender', 'math', 'computer',
-                      'paper', 'rocket', 'smell', 'orange']
-        self.key = []
+	self.init_words()
+	self.init_key()
 
+    def init_words(self):
+        self.words = ['tree', 'apple', 'circus', 'pig', 'face', 'mug', 'wind', 'stick', 'basket', 'picnic', 'string',
+                      'cloud', 'mouth', 'candy', 'fridge', 'table', 'iron', 'spoon', 'blender', 'math', 'napkin',
+                      'paper', 'rocket', 'smell', 'orange']
+
+    def init_key(self):
+	self.key = np.random.choice(Game.key_set, 25, False)
+ 
     def get_words(self):
         return self.words
 
-    def print_board(self):
+    def print_words(self):
         for i in range(5):
             row = ''
             for j in range(5):
                 row += '\t\t' + self.words[i*5 + j]
             print(row + '\n')
+
+    def get_key(self):
+	return self.key
+
+    def print_key(self):
+        for i in range(5):
+            row = ''
+            for j in range(5):
+                row += '\t\t' + self.key[i*5 + j]
+            print(row + '\n')
+
+
