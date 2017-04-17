05ec4808e60153aa1c72784b4c85bdc03eb7920d
David Freifeld <david.freifeld@analytics.adsrvr.org>
2017-04-17 02:38:56 +0000

    functionality for guessing words

--- Game.py
+++ Game.py
@@ -2,15 +2,18 @@
 # -*- utf-8 -*-
 
 import numpy as np
+import sys
 
 class Game:
 
     key_set = np.concatenate((np.repeat('blue', 9), np.repeat('red', 8), np.repeat('neutral', 7), ['assassin']))
+    player_set = ['blue', 'red']
 
     def __init__(self):
         # randomly generate some words and the key
 	self.init_words()
 	self.init_key()
+	self.init_board()
 
     def init_words(self):
         self.words = ['tree', 'apple', 'circus', 'pig', 'face', 'mug', 'wind', 'stick', 'basket', 'picnic', 'string',
@@ -20,6 +23,9 @@ class Game:
     def init_key(self):
 	self.key = np.random.choice(Game.key_set, 25, False)
  
+    def init_board(self):
+	self.board = list(self.words)
+
     def get_words(self):
         return self.words
 
@@ -32,6 +38,12 @@ class Game:
     def print_key(self):
 	self.print_array(self.key)
 
+    def get_board(self):
+	return self.board
+
+    def print_board(self):
+	self.print_array(self.board)
+
     def print_array(self, my_array):
 	for i in range(5):
             row = ''
@@ -39,9 +51,35 @@ class Game:
                 row += '\t\t' + my_array[i*5 + j]
             print(row + '\n')
 
+    def guess_word(self, board_index, player_index, guess_number, clue_number):
+
+	player_color = Game.player_set[player_index]
+	guess_color = self.key[board_index]
+
+	if guess_color == 'assassin':
+	    print(player_color + " player guessed the Assassin! The game is over.")
+	    sys.exit(0)
+
+	self.board[board_index] = guess_color 
+
+	if player_color == guess_color:
+	    return clue_number + 1 - guess_number
+
+	else:
+	    return 0 
+	
+
 if __name__ == "__main__":
     my_game = Game()
     print("Game words:")
     my_game.print_words()
     print("Game key:")
     my_game.print_key()
+    print("Game board:")
+    my_game.print_board()
+    print("After guessing index 4:")
+    guesses_remaining = my_game.guess_word(4, 0, 1, 2)
+    my_game.print_board()
+    print("Blue player has " + str(guesses_remaining) + " guesses remaining.")
+
+
