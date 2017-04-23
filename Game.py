2c38ede70715f71d6e8645ae44dbd2022ec9e035
David Freifeld <david.freifeld@analytics.adsrvr.org>
2017-04-23 05:01:27 +0000

    Adding in logic for storing guess and clue numbers and incrementing etc

--- Game.py
+++ Game.py
@@ -10,10 +10,16 @@ class Game:
     player_set = ['blue', 'red']
 
     def __init__(self):
-        # randomly generate some words and the key
+	
 	self.init_words()
 	self.init_key()
 	self.init_board()
+	self.init_current_player_turn()
+
+	self.current_guess_number = 0
+
+	self.current_clue_word  = None
+	self.current_clue_number = None
 
     def init_words(self):
         self.words = ['tree', 'apple', 'circus', 'pig', 'face', 'mug', 'wind', 'stick', 'basket', 'picnic', 'string',
@@ -23,6 +29,9 @@ class Game:
     def init_key(self):
 	self.key = np.random.choice(Game.key_set, 25, False)
  
+    def init_current_player_turn(self):
+	self.current_player_turn = 0
+
     def init_board(self):
 	self.board = list(self.words)
 
@@ -41,6 +50,18 @@ class Game:
     def get_board(self):
 	return self.board
 
+    def get_current_clue_word(self):
+	return self.current_clue_word
+
+    def get_current_clue_number(self):
+	return self.current_clue_number
+
+    def get_current_guesses_remaining(self):
+	return self.current_clue_number + 1 - self.current_guess_number
+
+    def print_current_clue(self):
+	print(self.current_clue_word + ", " + str(self.current_clue_number))
+
     def print_board(self):
 	self.print_array(self.board)
 
@@ -51,9 +72,13 @@ class Game:
                 row += '\t\t' + my_array[i*5 + j]
             print(row + '\n')
 
-    def guess_word(self, board_index, player_index, guess_number, clue_number):
+    def give_clue(self, clue_word, clue_number):
+	self.current_clue_word = clue_word
+	self.current_clue_number = clue_number
 
-	player_color = Game.player_set[player_index]
+    def guess_word(self, board_index):
+
+	player_color = Game.player_set[self.current_player_turn]
 	guess_color = self.key[board_index]
 
 	if guess_color == 'assassin':
@@ -63,10 +88,11 @@ class Game:
 	self.board[board_index] = guess_color 
 
 	if player_color == guess_color:
-	    return clue_number + 1 - guess_number
-
+	    self.current_guess_number = self.current_guess_number + 1
+	    return self.get_current_guesses_remaining() > 0
 	else:
-	    return 0 
+	    self.current_guess_number = self.current_clue_number + 1
+	    return False
 	
 
 if __name__ == "__main__":
@@ -77,9 +103,14 @@ if __name__ == "__main__":
     my_game.print_key()
     print("Game board:")
     my_game.print_board()
+    print("After giving a clue:")
+    my_game.give_clue("seed", 2)
+    my_game.print_current_clue()
+    print(my_game.get_current_guesses_remaining())
     print("After guessing index 4:")
-    guesses_remaining = my_game.guess_word(4, 0, 1, 2)
+    my_game.guess_word(4)
     my_game.print_board()
+    guesses_remaining = my_game.get_current_guesses_remaining()
     print("Blue player has " + str(guesses_remaining) + " guesses remaining.")
 
 
